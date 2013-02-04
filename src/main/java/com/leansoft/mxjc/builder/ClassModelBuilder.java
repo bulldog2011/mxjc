ae2620a6709542530f3176d5285d1aeb58536d68
bulldog2011 <51startup@sina.com>
2013-02-04 20:01:27 +0800

    remove any field

--- src/main/java/com/leansoft/mxjc/builder/ClassModelBuilder.java
+++ src/main/java/com/leansoft/mxjc/builder/ClassModelBuilder.java
@@ -171,6 +171,7 @@ public class ClassModelBuilder {
 					attrInfo.setValue(true);
 				} else if (cProp.kind() == PropertyKind.REFERENCE) {
 					attrInfo.setAny(true);
+					continue; // ignore any
 				}
 				
 				// Annotation
